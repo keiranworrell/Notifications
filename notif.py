ffeb8f7649de12e587b6a9dc59295e719f11f49c
keiranworrell <worrellkeiran@gmail.com>
2021-05-20 22:28:39 +0100

    Update notif.py

--- notif.py
+++ notif.py
@@ -13,29 +13,32 @@ def covid_data():
         # Data has not been found for some reason
         print("Request failed, please check internet connection")
 
-    if (covidData != None):
-        data=covidData.json()['Success']
+    if covidData != None:
+        data = covidData.json()["Success"]
         return data
+
 
 # send notification to system
 def push(title, message):
     sys = platform.system()
-    if (sys=='Darwin'):
-    	command = f'''
+    if sys == "Darwin":
+        command = f"""
     	osascript -e 'display notification "{message}" with title "{title}"'
-    	'''
-    elif (sys=='Linux'):
-    	command = f'''
+    	"""
+    elif sys == "Linux":
+        command = f"""
     	notify-send "{title}" "{message}"
-    	'''
+    	"""
     else:
-    	return	
+        return
     os.system(command)
+
 
 covidjson = covid_data()
 title = "UK Covid19 stats on {}".format(datetime.date.today())
 msg = "Total cases: {totalCases}\nNew cases today: {newCases}\nDeaths today :{newDeaths}".format(
-    totalCases = covidjson['cases'],
-    newCases = covidjson['todayCases'],
-    newDeaths = covidjson['todayDeaths'])
+    totalCases=covidjson["cases"],
+    newCases=covidjson["todayCases"],
+    newDeaths=covidjson["todayDeaths"],
+)
 push(title, msg)
